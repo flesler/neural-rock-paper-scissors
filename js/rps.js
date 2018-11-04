f678f30f1da976992b42d424ed2495373812a614
Ariel Flesler <aflesler@gmail.com>
2018-11-04 00:16:33 -0300

    Removed if they repeated choice as input

--- js/rps.js
+++ js/rps.js
@@ -19,7 +19,7 @@ const OPTIONS = ['rock', 'paper', 'scissors']
 
 const MEMORY_BLOCKS = 4
 const OUTPUTS = OPTIONS.length
-const INPUTS = OPTIONS.length + 2
+const INPUTS = OPTIONS.length + 1
 // Change to invalidate stored state
 const VERSION = 2
 
@@ -60,7 +60,7 @@ function beats(choice) {
 	return (choice + 1) % OPTIONS.length
 }
 
-ai.predict = function () {
+function predict() {
 	// Do a few random for learning
 	if (matches.length < MEMORY_BLOCKS) {
 		return Math.floor(Math.random() * OPTIONS.length)
@@ -88,8 +88,6 @@ function createMatch(choice, result, prev) {
 	}
 	// Track who won
 	input.push(result)
-	// Track if they picked the same option again
-	input.push(prev && prev.choice === choice ? 1 : 0)
 
 	if (input.length !== INPUTS) {
 		throw new Error('input size mismatch')
@@ -113,7 +111,7 @@ function startGame() {
 	showChoice('human', 0, false)
 	showChoice('ai', 0, false)
 	setState('thinking')
-	prediction = ai.predict()
+	prediction = predict()
 	setState('ready')
 }
 
